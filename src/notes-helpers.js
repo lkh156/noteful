c17e2b56f41f4c246f9e2aba5492a460c6baba50
lkh156 <66349051+lkh156@users.noreply.github.com>
2020-12-17 11:37:05 -0700

    Minor changes, looks good now.

---
+++ src/notes-helpers.js
@@ -0,0 +1,13 @@
+export const findFolder = (folders = [], folderId) =>
+  folders.find((folder) => folder.id === folderId);
+
+export const findNote = (notes = [], noteId) =>
+  notes.find((note) => parseInt(note.id) === parseInt(noteId));
+
+export const getNotesForFolder = (notes = [], folderId) =>
+  !folderId
+    ? notes
+    : notes.filter((note) => parseInt(note.folder_id) === parseInt(folderId));
+
+export const countNotesForFolder = (notes = [], folderId) =>
+  notes.filter((note) => note.folder_id === folderId).length;
